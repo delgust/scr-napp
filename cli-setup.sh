7a2acb987547cc1fabfe7bc05ba6d35c7ded0808
delgust <96631166+delgust@users.noreply.github.com>
2024-05-10 23:09:48 +0700

    Update cli-setup.sh

--- cli-setup.sh
+++ cli-setup.sh
@@ -5,8 +5,6 @@
 sudo apt-get update
 sudo apt-get upgrade -y
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-sudo touch /etc/apt/sources.list.d/kubernetes.list
-sudo echo 'deb http://apt.kubernetes.io/  kubernetes-xenial  main' | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
 sudo apt-get install -y kubectl=$k8sversion
 sudo apt-mark hold kubectl
