67b922bc954f8f40dcfe1f97b2299ae495bace4e
delgust <96631166+delgust@users.noreply.github.com>
2024-05-11 15:07:40 +0700

    Update cli-setup.sh

--- cli-setup.sh
+++ cli-setup.sh
@@ -4,7 +4,7 @@
 
 sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
 sudo mkdir /etc/apt/keyrings
-echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.24/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt update
 sudo apt-get install -y kubectl=$k8sversion
 sudo apt-mark hold kubectl
