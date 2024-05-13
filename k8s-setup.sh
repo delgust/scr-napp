d1437409218e24926b3ecc726b16b405693c629d
delgust <96631166+delgust@users.noreply.github.com>
2024-05-13 13:20:43 +0700

    Update k8s-setup.sh

--- k8s-setup.sh
+++ k8s-setup.sh
@@ -35,7 +35,6 @@ sudo systemctl restart docker
 
 #Install Kubernetes Packages
 sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
-echo "deb [signed-by=/etc/apt/keyrings/Kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.24/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.24/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.24/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt update
